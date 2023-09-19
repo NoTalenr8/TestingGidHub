fe43f57d98ac04c44335abf30c3a08fb795f5d2b
NoTalenr8 <pavkata.petrov0646@gmail.com>
2023-09-19 20:31:59 +0300

    Update Readme and cat.js

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # TestingGidHub
 
 This is my testing repository for SoftUni Lecture
+
+
+I am showing how to use GitHub life
--- cat.js
+++ cat.js
@@ -1,5 +1,12 @@
 //Marti car//
-let car = (
-  name:"Miro",
-  age: 9
+let car = {}
+  name: "Miro",
+  age; 9
+
+
+
+  let dog = {
+    name: 'My new dog ',
+    age: 2
+
   }
